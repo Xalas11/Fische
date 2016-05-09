41db58cafaeae9ef83e08f7fc2c2ba2297808492
bjarnedevel0per <bjarne.dasenbrook@me.com>
2016-05-09 17:24:18 +0200

    Fish Klasse

--- src/Fish.java
+++ src/Fish.java
@@ -2,4 +2,34 @@
  * Created by bjarne on 09.05.16.
  */
 public class Fish {
+    String name;
+    int price;
+    String[] incompa;
+    String[] compa;
+
+    public void Fisch(String name, int price, String[] incompa) {
+        this.name = name;
+        this. price = price;
+        this.incompa = incompa;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getIncompa() {
+        return incompa;
+    }
+
+    public String[] getCompa() {
+        return compa;
+    }
+
+    public void setCompa(String[] compa) {
+        this.compa = compa;
+    }
 }
